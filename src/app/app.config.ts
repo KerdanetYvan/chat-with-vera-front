c534e3a9faeee7acea01a86716cfffea4d663cef
KerdanetYvan <kerdanety@gmail.com>
2025-11-23 15:41:07 +0100

    feat: add Vercel Analytics integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideAppInitializer, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { inject as vercelAnalyticsInject } from '@vercel/analytics';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+
+    provideAppInitializer(() => {
+      // ⚠️ Vercel Analytics ne doit s’exécuter que dans le browser
+      if (typeof window !== 'undefined') {
+        vercelAnalyticsInject({
+          mode: isDevMode() ? 'development' : 'production',
+        });
+      }
+    }),
   ]
 };
