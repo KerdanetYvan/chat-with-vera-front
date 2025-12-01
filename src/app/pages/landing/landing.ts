212d2c70d79a2b78b2a1e2d9a52ce5c69ba50429
KerdanetYvan <kerdanety@gmail.com>
2025-12-01 16:31:01 +0100

    Initialisation: Image de fond + import navBar

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../core/services/theme.service';
+
+import { NavBar } from '../../components/nav-bar/nav-bar';
 
 @Component({
+  standalone: true,
   selector: 'app-landing',
-  imports: [],
+  imports: [NavBar],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
 export class Landing {
+  isDarkMode = false;
+
+  constructor(private theme: ThemeService) {
+    // Initialiser immédiatement
+    this.isDarkMode = this.theme.currentValue;
+
+    // S'abonner aux changements
+    this.theme.getDarkMode().subscribe(value => {
+      this.isDarkMode = value;
+    });
+  }  ngOnInit() {
+    // Autre logique d'initialisation si nécessaire
+  }
 
 }
