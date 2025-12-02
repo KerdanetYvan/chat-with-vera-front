2f8901fbd0ac658e8e73ad99a7a3c076aa7a931c
KerdanetYvan <kerdanety@gmail.com>
2025-12-02 01:30:04 +0100

    Add new icons and manifest for PWA support
    - Added icon files: icon-72x72.png and icon-96x96.png.
    - Created a web manifest file with app details and icon references.
    - Included multiple icon sizes for better compatibility across devices.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { inject as vercelAnalyticsInject } from '@vercel/analytics';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +19,9 @@ export const appConfig: ApplicationConfig = {
           mode: isDevMode() ? 'development' : 'production',
         });
       }
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
